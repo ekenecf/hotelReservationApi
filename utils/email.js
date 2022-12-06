58575e987ebfab3d5e4ec30090eb17b6d90245ce
ekenecf <ekens2u@gmail.com>
2022-12-06 16:48:54 +0100

    Create sendEmail function

--- utils/email.js
+++ utils/email.js
@@ -1,17 +1,27 @@
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
-const sendEmail = options => {
-    //First create a transporter which is a service such as gmail
-    const transporter = nodemailer.createTransport({
-        service: "Gmail",
-        auth: {
-            user:process.env.EMAIL,
-            password:process.env.EMAIL_PASSWORD
-        }
-    })
+const sendEmail = async (options) => {
+  //First create a transporter which is a service such as gmail
+  const transporter = nodemailer.createTransport({
+    // service: "Gmail",
     //to use gmail service you need to use the "less secure app" option
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      password: process.env.EMAIL_PASSWORD,
+    },
+  });
 
-    //define email options
-
-    //send email with nodeMailer
-}
+  //define email options
+  mailOptions = {
+    //specify where the email is coming from
+    from: "Ekene Nwachukwu <hello@ekene.com>",
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+  //send email with nodeMailer
+  await transporter.sendMail(mailOptions);
+};
+export default sendEmail;
